045d62d82ed94e7da6c62110a903a8985469b0d0
Danya <danya05af@gmail.com>
2024-10-16 12:15:04 +0300

    Three more tasks on the topic of 'Binary search'

---
+++ TaSD/Laboratory Work 3/G. Веревочки/G. Веревочки/G. Веревочки.cpp
@@ -0,0 +1,36 @@
+﻿// G. Веревочки
+#include <iostream>
+#include <vector>
+
+int good(int x, std::vector<int>& a, int k) {
+    int cnt = 0;
+    for (int j = 0; j < a.size(); j++) {
+        cnt = cnt + (a[j] / x);
+    }
+    return cnt >= k;
+}
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+
+    int q;
+    std::vector<int> arr;
+    for (int a = 0; a < n; a++) {
+        std::cin >> q;
+        arr.push_back(q);
+    }
+
+    int l = 0, r = 10000000 + 1;
+    while (r - l > 1) {
+        int m = (r + l) / 2;
+        if (good(m, arr, k)) {
+            l = m;
+        }
+        else {
+            r = m;
+        }
+    }
+    std::cout << l;
+}
---
+++ TaSD/Laboratory Work 3/H. Дипломы/H. Дипломы/H. Дипломы.cpp
@@ -0,0 +1,28 @@
+﻿// H. Дипломы
+#include <iostream>
+
+int good(long long x, long long w, long long h, long long n) {
+    return (x / w) * (x / h) >= n;
+}
+
+int main()
+{
+    long long w, h, n;
+    std::cin >> w >> h >> n;
+    long long l = 0;
+    long long r = 1;
+    while (!good(r, w, h, n)) {
+        r = r * 2;
+    }
+    while (r - l > 1) {
+        long long m = (l + r) / 2;
+        if (good(m, w, h, n)) {
+            r = m;
+        }
+        else {
+            l = m;
+        }
+    }
+    std::cout << r;
+}
+
